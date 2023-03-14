5bff07dffd18cabd19b6be7f621d9fb1d936a5cf
GregCrutchlow <gregcrutchlow@gmail.com>
2023-03-14 11:55:20 -0400

    added sass structure for styling, some logic for header and styling for content

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+const typedTextSpan = document.querySelector('.typed_text');
+
+const textArray = ['front end web developer.', 'student.', 'photographer.', 'dad.', 'nerd.'];
+const typingDelay = 100;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newTextDelay)
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay)
+    } else {
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 100);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(type, typingDelay);
+})
+
 document.querySelectorAll('.accordion_btn').forEach(button => {
     button.addEventListener('click', () => {
         const accordionContent = button.nextElementSibling;
