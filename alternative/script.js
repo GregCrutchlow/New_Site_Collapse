f21e27943acc1dcd50faa39356eb59c9c5024e50
GregCrutchlow <gregcrutchlow@gmail.com>
2023-03-16 15:42:15 -0400

    added styling to projects

--- alternative/script.js
+++ alternative/script.js
@@ -50,7 +50,7 @@ const myProjects = [
 	{
 		asset: './assets/guess-my-number-game-dev.png',
 		title: 'Guess My Number',
-		tools: '<li><i class="fa-brands fa-html5" aria-hidden="true"></i></li><li><i class="fa-brands fa-css3-alt" aria-hidden="true"></i></li><li><i class="fa-brands fa-js" aria-hidden="true"></i></li>',
+		tools: '<li><i class="fa-brands fa-html5" aria-hidden="true" title="HTML"></i></li><li><i class="fa-brands fa-css3-alt" aria-hidden="true" title="CSS"></i></li><li><i class="fa-brands fa-js" aria-hidden="true" title="JavaScript"></i></li>',
 		desc: 'Built using vanilla JavaScript, Guess My Number is a number guessing game between 1-20. Simple code to test my skills manipulating the DOM as well as some game coding and error handling skills.',
 		liveURL: 'https://guess-my-number-game-dev.netlify.app',
 		repoURL: 'https://github.com/GregCrutchlow/Guess-My-Number',
@@ -58,7 +58,7 @@ const myProjects = [
 	{
 		asset: './assets/pig-game.png',
 		title: 'Pig Game',
-		tools: '<li><i class="fa-brands fa-html5" aria-hidden="true"></i></li><li><i class="fa-brands fa-css3-alt" aria-hidden="true"></i></li><li><i class="fa-brands fa-js" aria-hidden="true"></i></li>',
+		tools: '<li><i class="fa-brands fa-html5" aria-hidden="true"></i></li><li><i class="fa-brands fa-css3-alt" aria-hidden="true"></i></li><li><i class="fa-brands fa-js" aria-hidden="true" title="JavaScript"></i></li>',
 		desc: 'Built using vanilla JavaScript, Pig is a dice rolling game where the goal is to bank 100 points before your opponent. Players can score big but lose even bigger in this game of risk/reward. Built using simple JavaScript for game functionality.',
 		liveURL: 'https://pig-game-gc-dev.netlify.app',
 		repoURL: 'https://github.com/GregCrutchlow/pig-game',
@@ -66,7 +66,7 @@ const myProjects = [
 	{
 		asset: './assets/isthebookbetter44.png',
 		title: 'Is The Book Better',
-		tools: '<li><i class="devicon-react-original" aria-hidden="true"></i></li><li><i class="devicon-nodejs-plain" aria-hidden="true"></i></li><li><i class="devicon-sass-original" aria-hidden="true"></i></li><li><i class="fa-brands fa-js" aria-hidden="true"></i></li><li><i class="fa-solid fa-people-group" aria-hidden="true"></i></li>',
+		tools: '<li><i class="devicon-react-original" aria-hidden="true"></i></li><li><i class="devicon-nodejs-plain" aria-hidden="true"></i></li><li><i class="devicon-sass-original" aria-hidden="true"></i></li><li><i class="fa-brands fa-js" aria-hidden="true" title="JavaScript"></i></li><li><i class="fa-solid fa-people-group" aria-hidden="true"></i></li>',
 		desc: 'Built during bootcamp at Juno College cohort 44. Group project with other members Paco, Louis and Matt. Compare a book and its movie and find out which one you should spend your time on!',
 		liveURL: 'https://isthebookbetter44.netlify.app',
 		repoURL: 'https://github.com/book-or-movie/is-the-book-better/tree/production',
@@ -82,28 +82,28 @@ const myProjects = [
 ];
 
 const populateProjects = () => {
-    const projectSectionEl = document.querySelector('#projects .wrapper');
+    const projectSectionEl = document.querySelector('.projects');
 
     myProjects.forEach(project => {
         const projectContainer = document.createElement('article');
+        projectContainer.classList.add('project_style');
         projectContainer.innerHTML = `
-            <div className="projectImg">
-                <a href="${project.liveURL}"><img src="${project.asset}" alt="${project.title}" /></a>
+            <div class="project_img">
+                <a href="${project.liveURL}"><img title="${project.title}" src="${project.asset}" alt="${project.title}"/></a>
             </div>
-            <div className="project_desc">
+            <div class="project_desc">
                 <h3>${project.title}</h3>
                 <p>${project.desc}</p>
-                <div className="skills_used">
+                <div class="skills_used">
                     <ul>
                         ${project.tools}
                     </ul>
                     <ul>
-                        <li><a href="${project.live
-                        }"><button>Live Site</button></a></li>
-                        <li><a href="${project.repoURL}"><button>GitHub</button></a></li>
+                        <li><a href="${project.liveURL}" target="_blank" rel="noopener noreferrer"><button>Live Site</button></a></li>
+                        <li><a href="${project.repoURL}" target="_blank" rel="noopener noreferrer"><button>GitHub</button></a></li>
                     </ul>
                 </div>
-            </div>`
+            </div>`;
         projectSectionEl.appendChild(projectContainer);
     })
 }
